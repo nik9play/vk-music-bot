dc3b1d80019caa9a5868921f9f437a7a9b85cbd8
nik9play <n-bogun@mail.ru>
2020-07-14 20:39:15 +0300

    удаление из очереди

---
+++ commands/vr.js
@@ -0,0 +1,21 @@
+export default {
+  name: "vr",
+  description: "Удалить трек/треки из очереди. Например: `-vr 3-8`",
+  execute: async function(message, args, options) {
+    if (!options.serverQueue) return
+    const oldLength = options.serverQueue.songs.length
+    args.forEach(a => {
+      if (a.includes("-")) {
+        const first = parseInt(a.split("-")[0])
+        const last = parseInt(a.split("-")[1])
+        
+        if ((last > first) && (first > 1)) options.serverQueue.songs.splice(first-1, last-first+1)
+      } else {
+        a = parseInt(a)
+        if (a > 1) options.serverQueue.songs.splice(a-1, 1)
+      }
+    })
+    const newLength = options.serverQueue.songs.length
+    message.reply(`удалено треков: \`${oldLength - newLength}\`.`)
+  }
+}
