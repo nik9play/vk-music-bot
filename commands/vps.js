4a0477373dc7b55daf2f3b34510ef6d60ca2a2d1
nik9play <n-bogun@mail.ru>
2020-08-02 00:25:08 +0300

    фикс паузы для динамичного подключения

--- commands/vps.js
+++ commands/vps.js
@@ -2,26 +2,23 @@ export default {
   name: "vps",
   description: "Пауза и воспроизведение",
   execute: async function(message, _args, options) {
-    const voiceChannel = message.member.voice.channel
-    if (!voiceChannel) return message.reply('вы должны быть в голосовом канале чтобы поставить музыку на паузу.')
-    if (!options.serverQueue) return
-    if (!options.serverQueue.connection.dispatcher.paused) {
-      options.serverQueue.connection.dispatcher.pause()
-      const id = message.guild.id
-      options.serverQueue.exitTimer = setTimeout(async () => {
-        if (!options.enable247List.has(message.guild.id)) {
-          const serverQueueNew = options.queue.get(id)
-          if (!serverQueueNew) return
-          await serverQueueNew.connection.dispatcher.resume()
-          serverQueueNew.songs = []
-          serverQueueNew.connection.dispatcher.end()
+    const voiceConnection = message.client.voice.connections.get(message.guild.id)
+
+    if (voiceConnection)
+      if (voiceConnection.dispatcher)
+        if (!voiceConnection.dispatcher.paused) {
+          voiceConnection.dispatcher.pause()
+          options.serverQueue.exitTimer = setTimeout(async () => {
+            if (!options.enable247List.has(message.guild.id)) {
+              const voiceConnection = message.client.voice.connections.get(message.guild.id)
+              voiceConnection.channel.leave()
+            }
+          }, 3600000)
+          message.react('⏸️')
+        } else {
+          voiceConnection.dispatcher.resume()
+          message.react('▶️')
+          if (options.serverQueue.exitTimer) clearTimeout(options.serverQueue.exitTimer)
         }
-      }, 3600000)
-      message.react('⏸️')
-    } else {
-      options.serverQueue.connection.dispatcher.resume()
-      message.react('▶️')
-      if (options.serverQueue.exitTimer) clearTimeout(options.serverQueue.exitTimer)
-    }
   }
 } 
