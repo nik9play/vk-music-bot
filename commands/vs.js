533adb0c73f996a1923d2bc9d14e445a87dfdbf5
nik9play <n-bogun@mail.ru>
2020-08-02 13:42:10 +0300

    фикс под динамичное подключение

--- commands/vs.js
+++ commands/vs.js
@@ -2,14 +2,14 @@ export default {
   name: "vs",
   description: "Выключить музыку и очистить очередь",
   execute: async function(message, _args, options) {
-    const voiceChannel = message.client.voice.connections.get(message.guild.id)
+    const voiceConnection = message.client.voice.connections.get(message.guild.id)
 
-    if (!voiceChannel) return message.reply('бот не в голосовом канале.')
+    if (!voiceConnection) return message.reply('бот не в голосовом канале.')
     if (!options.serverQueue) return message.reply('нечего останавливать.')
-    
-    await options.serverQueue.connection.dispatcher.resume()
+
+    await voiceConnection.dispatcher.resume()
     options.serverQueue.songs = []
-    options.serverQueue.connection.dispatcher.end()
+    voiceConnection.dispatcher.end()
     message.react('⏹️')
   }
 }
