940a5a1668b8224761f84358430532a9ac819045
nik9play <n-bogun@mail.ru>
2020-08-12 20:14:42 +0300

    добавлена поддержка другого типа урл плейлистов

--- tools/parsePlaylistURL.js
+++ tools/parsePlaylistURL.js
@@ -8,15 +8,31 @@ export default function(url) {
     }
   }
   
-  const query = url.searchParams.get("z")
-  let id = null, access_key = null
-  if (query) {
-    id = query.split("/")[0].replace("audio_playlist", "")
-    access_key = query.split("/")[1]
+  if (url.pathname.includes("/music/playlist/")) {
+    const query = url.pathname.split("/")[3]
+    let id = null, access_key
+
+    if (query) {
+      id = query
+    }
+
+    return {
+      id,
+      access_key
+    }
+  } else {
+    const query = url.searchParams.get("z")
+    let id = null, access_key = null
+
+    if (query) {
+      id = query.split("/")[0].replace("audio_playlist", "")
+      access_key = query.split("/")[1]
+    }
+  
+    return {
+      id,
+      access_key
+    }
   }
 
-  return {
-    id,
-    access_key
-  }
 }
