bd1165532c589240fe29cd6e7b21a94c5c11864f
nik9play <n-bogun@mail.ru>
2020-08-12 20:15:29 +0300

    фиксы апи

--- vkapi.js
+++ vkapi.js
@@ -8,172 +8,6 @@ const axiosHeaders = {
   "User-Agent": "KateMobileAndroid/56 lite-460 (Android 4.4.2; SDK 19; x86; unknown Android SDK built for x86; en)"
 }
 
-// async function audioSearchOne(query, captcha) {
-//   console.log(encodeURI(`${vkApiLink}search${connectString}&q=${query}&count=1${captcha ? `&captcha_sid=${captcha.sid}&captcha_key=${captcha.key}` : ``}`))
-//   const res = await fetch(encodeURI(`${vkApiLink}search${connectString}&q=${query}&count=1${captcha ? `&captcha_sid=${captcha.sid}&captcha_key=${captcha.key}` : ``}`), {
-//     headers: {
-//       "User-Agent": "KateMobileAndroid/56 lite-460 (Android 4.4.2; SDK 19; x86; unknown Android SDK built for x86; en)"
-//     }
-//   })
-
-//   if (res.ok) {
-//     const json = await res.json()
-    
-//     if (!json.error) {
-//       if (json.response.count != 0) {
-//         return {
-//           status: "success",
-//           songInfo: {
-//             title: json.response.items[0].title,
-//             artist: json.response.items[0].artist,
-//             url: json.response.items[0].url,
-//             duration: json.response.items[0].duration
-//           }
-//         }
-//       } else {
-
-//         const IDres = await fetch(encodeURI(`${vkApiLink}getById${connectString}&audios=${query}`), {
-//           headers: {
-//             "User-Agent": "KateMobileAndroid/56 lite-460 (Android 4.4.2; SDK 19; x86; unknown Android SDK built for x86; en)"
-//           }
-//         })
-
-//         const IDjson = await IDres.json()
-
-//         if (IDjson.response) {
-//           if (IDjson.response.length > 0) return {
-//             status: "success",
-//             songInfo: {
-//               title: IDjson.response[0].title,
-//               artist: IDjson.response[0].artist,
-//               url: IDjson.response[0].url,
-//               duration: IDjson.response[0].duration
-//             }
-//           }
-//           else return {
-//             status: "error",
-//             message: "empty-api"
-//           }
-//         }
-//         return {
-//           status: "error",
-//           message: "empty-api"
-//         }
-//       }
-
-//     } else {
-//       return {
-//         status: "error",
-//         message: "fail-api",
-//         details: json.error
-//       }
-//     }
-//   } else {
-//     return {
-//       status: "error",
-//       message: "fail-fetch"
-//     }
-//   }
-// }
-
-// async function audioGetPlaylввist(owner_id, album_id, count, offset, access_key, captcha) {
-//   const res = await fetch(encodeURI(`${vkApiLink}get${connectString}&count=10&offset=${offset - 1}&owner_id=${owner_id}&album_id=${album_id}&count=${count}${captcha ? `&captcha_sid=${captcha.sid}&captcha_key=${captcha.key}` : ``}${access_key ? `&access_key=${access_key}` : ``}`), {
-//     headers: {
-//       "User-Agent": "KateMobileAndroid/56 lite-460 (Android 4.4.2; SDK 19; x86; unknown Android SDK built for x86; en)"
-//     }
-//   })
-
-//   if (res.ok) {
-//     const json = await res.json()
-    
-//     if (!json.error) {
-//       if (json.response.count != 0) {
-//         let newArray = []
-//         json.response.items.forEach(e => {
-//           newArray.push({
-//             title: e.title,
-//             artist: e.artist,
-//             url: e.url,
-//             duration: e.duration
-//           })
-//         })
-  
-//         return {
-//           status: "success",
-//           count: json.response.count,
-//           newArray: newArray
-//         }
-//       } else {
-//         return {
-//           status: "error",
-//           message: "empty-api"
-//         }
-//       }
-//     } else {
-//       return {
-//         status: "error",
-//         message: "fail-api",
-//         details: json.error
-//       }
-//     }
-//   } else {
-//     return {
-//       status: "error",
-//       message: "fail-fetch"
-//     }
-//   }
-// }
-
-// async function audioGetUsввer(owner_id, count, offset, captcha) {
-//   const res = await fetch(encodeURI(`${vkApiLink}get${connectString}&count=10&offset=${offset - 1}&owner_id=${owner_id}&count=${count}${captcha ? `&captcha_sid=${captcha.sid}&captcha_key=${captcha.key}` : ``}`), {
-//     headers: {
-//       "User-Agent": "KateMobileAndroid/56 lite-460 (Android 4.4.2; SDK 19; x86; unknown Android SDK built for x86; en)"
-//     }
-//   })
-
-//   if (res.ok) {
-//     const json = await res.json()
-    
-//     if (!json.error) {
-//       if (json.response.count != 0) {
-//         let newArray = []
-//         json.response.items.forEach(e => {
-//           newArray.push({
-//             title: e.title,
-//             artist: e.artist,
-//             url: e.url,
-//             duration: e.duration
-//           })
-//         })
-  
-//         return {
-//           status: "success",
-//           count: json.response.count,
-//           newArray: newArray
-//         }
-//       } else {
-//         return {
-//           status: "error",
-//           message: "empty-api"
-//         }
-//       }
-//     } else {
-//       return {
-//         status: "error",
-//         message: "fail-api",
-//         details: json.error
-//       }
-//     }
-//   } else {
-//     return {
-//       status: "error",
-//       message: "fail-fetch"
-//     }
-//   }
-// }
-
-
-
 async function tryID(query, http, urlSearch) {
   urlSearch.append("audios", query)
 
@@ -286,7 +120,7 @@ async function audioGetPlaylist(owner_id, album_id, count, offset, access_key, c
       return handleError(req.data.error)
     }
 
-    if (req.data.response.count == 0) {
+    if (req.data.response.items.length == 0) {
       return {
         status: "error",
         type: "empty"
@@ -338,7 +172,7 @@ async function audioGetUser(owner_id, count, offset, captcha, http) {
       return handleError(req.data.error)
     }
 
-    if (req.data.response.count == 0) {
+    if (req.data.response.items.length == 0) {
       return {
         status: "error",
         type: "empty"
@@ -367,7 +201,59 @@ async function audioGetUser(owner_id, count, offset, captcha, http) {
   }
 }
 
+// async function search(query, count, offset, captcha, http) {
+//   let urlSearch = getUrlDefaultParams()
+
+//   if (captcha) {
+//     urlSearch.append("captcha_sid", captcha.sid)
+//     urlSearch.append("captcha_key", captcha.key)
+//   }
+
+//   urlSearch.append("q", query)
+//   urlSearch.append("offset", offset)
+//   urlSearch.append("count", count)
+
+//   try {
+//     const req = await http.get(`${vkApiLink}get`, {
+//       params: urlSearch,
+//       headers: axiosHeaders
+//     })
+
+//     if (req.data.error) {
+//       return handleError(req.data.error)
+//     }
+
+//     if (req.data.response.count == 0) {
+//       return {
+//         status: "error",
+//         type: "empty"
+//       }
+//     }
+
+//     let newArray = []
+//     req.data.response.items.forEach(e => {
+//       newArray.push({
+//         title: e.title,
+//         artist: e.artist,
+//         url: e.url,
+//         duration: e.duration
+//       })
+//     })
+
+//     return {
+//       status: "success",
+//       newArray
+//     }
+//   } catch {
+//     return {
+//       status: "error",
+//       type: "request"
+//     }
+//   }
+// }
+
 function handleError(data) {
+  console.log(data)
   if (data.code == 14) {
     return {
       status: "error",
