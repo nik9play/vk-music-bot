3c68063e564478bd1a62fd3dcd9bec71eef8a0e3
nik9play <n-bogun@mail.ru>
2020-08-13 18:59:04 +0300

    фикс возможной ошибки с пропуском трека

--- commands/vn.js
+++ commands/vn.js
@@ -8,8 +8,11 @@ export default {
     if (!voiceConnection) return message.reply('бот не в голосовом канале.')
     if (!options.serverQueue) return message.reply('некуда пропускать.')
 
-    await voiceConnection.dispatcher.resume()
-    voiceConnection.dispatcher.end()
+    if (voiceConnection.dispatcher) {
+      await voiceConnection.dispatcher.resume()
+      voiceConnection.dispatcher.end()
+    }
+    
     message.react('⏭️')
   }
 } 
