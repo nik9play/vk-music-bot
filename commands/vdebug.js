dbd29472b23b4ceb27b345e091f19ed5ef258215
nik9play <n-bogun@mail.ru>
2020-08-15 15:34:37 +0300

    обновление дебаг команды

--- commands/vdebug.js
+++ commands/vdebug.js
@@ -1,9 +1,20 @@
 export default {
   name: "vdebug",
-  cooldown: 3,
+  cooldown: 10,
   execute: async function (message, _args, options) {
     console.log(options.queue.get(message.guild.id))
     const voiceConnection = message.guild.client.voice.connections.get(message.guild.id)
-    console.log(voiceConnection)
+
+    if (voiceConnection) {
+      console.log(voiceConnection)
+      if (voiceConnection.channel) {
+        const voicePermissions = voiceConnection.channel.permissionsFor(message.client.user)
+        console.log(`Права для голосового канал: ${voicePermissions.toArray()}`)
+      }
+    }
+
+    const textPermissions = message.channel.permissionsFor(message.client.user)
+    console.log(`Права для текстового канал: ${textPermissions.toArray()}`)
+    message.reply("дебаг инфа отправлена в консоль")
   }
 }
