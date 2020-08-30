a6511f3167fba4ac1fa0f6ffab6ef4f53f6e74c8
nik9play <n-bogun@mail.ru>
2020-08-31 00:50:06 +0300

    команда для зацикливания

---
+++ commands/vl.js
@@ -0,0 +1,32 @@
+export default {
+  name: "vl",
+  description: "Включает зацикливание очереди (`-vl queue`) или трека (`-vl song`). Чтобы выключить, используйте `-vl off`",
+  cooldown: 2,
+  execute: async function(message, args, options) {
+    if (args[0] == "queue") {
+      const serverQueue = options.queue.get(message.guild.id)
+      if (!serverQueue) return message.reply("нечего зацикливать")
+
+      serverQueue.loopType = 1
+      serverQueue.loopSongs = [...serverQueue.songs]
+
+      message.reply("включено зацикливание очереди")
+    } else if (args[0] == "song") {
+      const serverQueue = options.queue.get(message.guild.id)
+      if (!serverQueue) return message.reply("нечего зацикливать")
+
+      serverQueue.loopType = 2
+      serverQueue.loopSongs[0] = serverQueue.songs[0]
+
+      message.reply("включено зацикливание текущего трека")
+    } else if (args[0] == "off") {
+      const serverQueue = options.queue.get(message.guild.id)
+      if (!serverQueue) return
+
+      serverQueue.loopType = 0
+      serverQueue.loopSongs = []
+
+      message.reply("зацикливание выключено")
+    }
+  }
+}
