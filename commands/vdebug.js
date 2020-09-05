3b1a8d1c13f9fce564a6515a46784d8279db7624
nik9play <n-bogun@mail.ru>
2020-09-05 14:00:06 +0300

    изменение команды дебага для lavalink

--- commands/vdebug.js
+++ commands/vdebug.js
@@ -3,15 +3,6 @@ export default {
   cooldown: 10,
   execute: async function (message, _args, options) {
     console.log(options.queue.get(message.guild.id))
-    const voiceConnection = message.guild.client.voice.connections.get(message.guild.id)
-
-    if (voiceConnection) {
-      console.log(voiceConnection)
-      if (voiceConnection.channel) {
-        const voicePermissions = voiceConnection.channel.permissionsFor(message.client.user)
-        console.log(`Права для голосового канал: ${voicePermissions.toArray()}`)
-      }
-    }
 
     const textPermissions = message.channel.permissionsFor(message.client.user)
     console.log(`Права для текстового канал: ${textPermissions.toArray()}`)
