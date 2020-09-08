54995713eecba933a6f40993f24087beeaf7047d
nik9play <n-bogun@mail.ru>
2020-09-08 14:36:51 +0300

    удаление таймера в vfix

--- commands/vfix.js
+++ commands/vfix.js
@@ -3,6 +3,12 @@ export default {
   description: "Перезапускает бота если что-то не так.",
   cooldown: 20,
   execute: async function(message, _args, options) {
+    const queue = options.queue.get(message.guild.id)
+
+    if (queue)
+      if (queue.pauseTimer)
+        clearTimeout(queue.pauseTimer)
+
     options.queue.delete(message.guild.id)
     const player = options.shoukaku.getPlayer(message.guild.id)
     if (player) {
