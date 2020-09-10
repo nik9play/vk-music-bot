578ef12bd7efcc6d3607a99f6fb3e31cacd673cd
nik9play <n-bogun@mail.ru>
2020-09-10 13:13:41 +0300

    фикс пропуска трека при паузе

--- commands/vn.js
+++ commands/vn.js
@@ -1,10 +1,14 @@
+import vps from "./vps"
+
 export default {
   name: "vn",
   description: "Пропустить трек",
   cooldown: 3,
-  execute: async function skip(message, _args, options) {
+  execute: async function skip(message, args, options) {
     const player = options.shoukaku.getPlayer(message.guild.id)
     if (player) {
+      if (player.paused) vps.execute(message, args, options)
+
       player.stopTrack()
     
       const textPermissions = message.channel.permissionsFor(message.client.user)
