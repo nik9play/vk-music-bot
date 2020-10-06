9f34cc67a7bc74c6506fb93cbf8a6b43260ee674
nik9play <n-bogun@mail.ru>
2020-10-06 19:46:44 +0300

    fix vr

--- commands/vr.js
+++ commands/vr.js
@@ -3,20 +3,21 @@ export default {
   description: "Удалить трек/треки из очереди. Например: `-vr 3-8` или `-vr 3 5 6-8`",
   cooldown: 3,
   execute: async function(message, args, options) {
-    if (!options.serverQueue) return
-    const oldLength = options.serverQueue.songs.length
+    const serverQueue = options.queue.get(message.guild.id)
+    if (!serverQueue) return message.reply("нечего удалять.")
+    const oldLength = serverQueue.songs.length
     args.forEach(a => {
       if (a.includes("-")) {
         const first = parseInt(a.split("-")[0])
         const last = parseInt(a.split("-")[1])
         
-        if ((last > first) && (first > 1)) options.serverQueue.songs.splice(first-1, last-first+1)
+        if ((last > first) && (first > 1)) serverQueue.songs.splice(first-1, last-first+1)
       } else {
         a = parseInt(a)
-        if (a > 1) options.serverQueue.songs.splice(a-1, 1)
+        if (a > 1) serverQueue.songs.splice(a-1, 1)
       }
     })
-    const newLength = options.serverQueue.songs.length
+    const newLength = serverQueue.songs.length
     message.reply(`удалено треков: \`${oldLength - newLength}\`.`)
   }
 }
