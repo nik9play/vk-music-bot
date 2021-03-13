c5bb326c9ad7731e7b08356727fd6f8a7b3a45e6
Nikita <n-bogun@mail.ru>
2021-03-13 15:32:56 +0300

    изменен способ удаления сообщения

--- src/commands/247.js
+++ src/commands/247.js
@@ -19,7 +19,7 @@ export default {
             message.channel.send({embed: {
               description: `**Я покинул канал, так как слишком долго был неактивен.** Хотите, чтобы я оставался? Включите режим 24/7 (доступен только для Премиум пользователей). `,
               color: 0x5181b8
-            }}).then(msg => setTimeout(() => msg.delete(), 30000))
+            }}).then(msg => msg.delete({timeout: 30000}))
           }, 1200000))
     }
   }
