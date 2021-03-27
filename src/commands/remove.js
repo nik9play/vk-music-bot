7a3ce0e03c6698ecafbf69306c91818a7d3b9860
Nikita <n-bogun@mail.ru>
2021-03-27 15:06:58 +0300

    фикс команды удаления

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -8,20 +8,20 @@ export default {
 
     const queue = player.queue
 
-    let tracksRemoved = 0
+    let removedTracks = []
 
     args.forEach(a => {
       if (a.includes("-")) {
         const first = parseInt(a.split("-")[0])
         const last = parseInt(a.split("-")[1])
         
-        if ((last > first)) tracksRemoved += queue.remove(first, last)
+        if ((last > first)) removedTracks = [...removedTracks, ...queue.remove(first-1, last)]
       } else {
         a = parseInt(a)
-        if (a > 1) tracksRemoved += queue.remove(a)
+        if (a > 1) removedTracks = [...removedTracks, ...queue.remove(a-1)]
       }
     })
 
-    message.reply(`удалено треков: ${tracksRemoved}.`)
+    message.reply(`удалено треков: ${removedTracks.length}.`)
   }
 }
