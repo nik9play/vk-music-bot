b92c3910530f00a122771a04a1f0772e54ca8bb3
Nikita <n-bogun@mail.ru>
2021-03-27 17:52:21 +0300

    временно отключить команду для мэшапа

--- src/commands/mashup.js
+++ src/commands/mashup.js
@@ -1,11 +1,12 @@
-import mashupList from '../lists/mashup.json'
-import play from './play'
+// import mashupList from '../lists/mashup.json'
+// import play from './play'
 
 export default {
   name: "mashup",
   djOnly: true,
-  execute: async (message, _args, options) => { 
-    const id = mashupList[Math.floor(Math.random() * mashupList.length)]
-    play.execute(message, [id], options)
+  execute: async (message) => { 
+    // const id = mashupList[Math.floor(Math.random() * mashupList.length)]
+    // play.execute(message, [id], options)
+    message.reply("команда для включения случайного мэшапа временно недоступна.")
   }
 }
