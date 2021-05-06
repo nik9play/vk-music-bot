b2820890fd601c99e9c4ff5fa5b2642ead7d26ff
Nikita <n-bogun@mail.ru>
2021-05-07 00:27:00 +0300

    clean dist folder on build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import json from '@rollup/plugin-json';
 import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
+import del from 'rollup-plugin-delete'
 
 export default {
   input: './src/index.js',
@@ -7,5 +8,6 @@ export default {
     dir: './dist/',
     format: 'cjs'
   },
-  plugins: [dynamicImportVars(), json()]
+  plugins: [dynamicImportVars(), json(),
+  del({targets: 'dist/*'})]
 };
