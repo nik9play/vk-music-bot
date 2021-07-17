be190418bf0fdb406efc3a1f9057579b932cfd19
nik9 <n-bogun@mail.ru>
2021-07-17 23:17:41 +0300

    функция для генерации ошибок

--- src/tools/generateErrorMessage.js
+++ src/tools/generateErrorMessage.js
@@ -1,21 +1,25 @@
 export default function(message, type='error') {
   let title
-
+  let color
+  
   switch (type) {
     case 'error':
       title = '<:no2:835498572916195368> **Ошибка!**'
+      color = 0xED4245
       break
     case 'warning':
       title = '⚠️ **Предупреждение**'
+      color = 0xFEE75C
       break
     case 'info':
       title = 'ℹ️ **Информация**'
+      color = 0x3b88c3
   }
   
   const embed = {
     // title: `<:no2:835498572916195368> ${title}`,
     description: `${title}\n${message}`,
-    color: 0xED4245
+    color
   }
 
   return embed
