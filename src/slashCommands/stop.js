035d3b0bef3120c0568c9916b923581650a9f22b
nik9 <n-bogun@mail.ru>
2021-08-16 22:36:45 +0300

    изменен эмбед

--- src/slashCommands/stop.js
+++ src/slashCommands/stop.js
@@ -15,6 +15,6 @@ export default {
     player.queue.clear()
     player.stop()
   
-    respond({ embeds: [{ description: '⏹️ Воспроизведение остановлено и очередь очищена.', color: 0x5181b8 }] }) 
+    respond({embeds: [generateErrorMessage('⏹️ Воспроизведение остановлено и очередь очищена.', 'notitle')]}) 
   }
 }
