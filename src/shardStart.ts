aeac367bc0bd304def2057a4a56bd6a248d73fd6
Nikita <n-bogun@mail.ru>
2022-06-27 21:32:37 +0300

    change timeout

--- src/shardStart.ts
+++ src/shardStart.ts
@@ -23,7 +23,7 @@ manager.on('clusterCreate', cluster => {
 
 manager.on('debug', msg => logger.info(msg, 'CLUSTER MANAGER'))
 
-manager.spawn({ timeout: 120000 }).then(() => {
+manager.spawn({ timeout: 240000 }).then(() => {
   logger.info(`Manager finished spawning clusters. Total clusters: ${manager.totalClusters}`)
   setTimeout(() => {
     sendInfo()
