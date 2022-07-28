a501580e2f86270b655abe64a6a96fec3e8c745d
Nikita <n-bogun@mail.ru>
2022-07-28 18:29:03 +0300

    фикс выхода из канала

--- src/Utils.ts
+++ src/Utils.ts
@@ -157,36 +157,36 @@ export default class Utils {
   }
 
   public static getExitTimeout(player: Player, client: VkMusicBotClient): NodeJS.Timeout {
+    logger.info(`Exit timeout set ${player.guild}`)
+    
     return setTimeout(async () => {
-      // if (player) {
-      //   logger.info(`Exit timeout set ${player.guild}`)
+      if (player) {
+        player.destroy()
 
-      //   player.destroy()
+        // if (player.textChannel == null) return
 
-      //   if (player.textChannel == null) return
+        // const channel = client.channels.cache.get(player.textChannel)
 
-      //   const channel = client.channels.cache.get(player.textChannel)
+        // if (channel?.isText()) {
+        //   try {
+        //     const message = await channel.send({
+        //       embeds: [{
+        //         description: '**Я покинул канал, так как слишком долго был неактивен.**\n Хотите, чтобы я оставался? Включите режим 24/7 (доступен только для Премиум пользователей, подробности: `/donate`). ',
+        //         color: 0x5181b8
+        //       }]
+        //     })
 
-      //   if (channel?.isText()) {
-      //     try {
-      //       const message = await channel.send({
-      //         embeds: [{
-      //           description: '**Я покинул канал, так как слишком долго был неактивен.**\n Хотите, чтобы я оставался? Включите режим 24/7 (доступен только для Премиум пользователей, подробности: `/donate`). ',
-      //           color: 0x5181b8
-      //         }]
-      //       })
+        //     setTimeout(() => {
+        //       if (message && typeof message.delete === 'function') {
+        //         message.delete().catch(err => logger.error({ err }, 'Can\'t delete message'))
+        //       }
+        //     }, 30000)
+        //   } catch (err) {
+        //     logger.error({ err }, 'Can\'t send timeout message')
+        //   }
 
-      //       setTimeout(() => {
-      //         if (message && typeof message.delete === 'function') {
-      //           message.delete().catch(err => logger.error({ err }, 'Can\'t delete message'))
-      //         }
-      //       }, 30000)
-      //     } catch (err) {
-      //       logger.error({ err }, 'Can\'t send timeout message')
-      //     }
-
-      //   }
-      // }
+        // }
+      }
     }, 1200000)
   }
 
