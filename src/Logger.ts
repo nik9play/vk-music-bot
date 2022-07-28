5355a8ab1b4d619d7b9462ce2717de674a43a89f
Nikita <n-bogun@mail.ru>
2022-07-29 02:00:30 +0300

    async logger

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,6 +1,9 @@
 import pino from 'pino'
 
-const logger = pino()
+const logger = pino(pino.destination({
+  minLength: 1024,
+  sync: false
+}))
 logger.info('Logger initialized.')
 
 export default logger
