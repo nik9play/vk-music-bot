da4b37cf294a3127edacd96faec832051cca4445
Nikita <n-bogun@mail.ru>
2022-10-12 18:54:41 +0300

    команда text

---
+++ src/slashCommands/text.ts
@@ -0,0 +1,92 @@
+import { Command } from '../SlashCommandManager'
+import Utils, { ErrorMessageType } from '../Utils'
+
+export default new Command({
+  name: 'text',
+  djOnly: true,
+  adminOnly: false,
+  premium: false,
+  cooldown: 1,
+  execute: async ({ client, guild, respond, voice, text, interaction }) => {
+    const player = client.manager.get(guild.id)
+    if (!player) {
+      await respond({
+        embeds: [Utils.generateErrorMessage('Сейчас ничего не играет.')],
+        ephemeral: true
+      })
+      return
+    }
+
+    if (!voice) {
+      await respond({
+        embeds: [
+          Utils.generateErrorMessage(
+            'Необходимо находиться в голосовом канале.'
+          )
+        ],
+        ephemeral: true
+      })
+      return
+    }
+    //if (channel.id !== player.voiceChannel) return message.reply("вы находитесь не в том голосовом канале.")
+
+    let textChannel = text
+    const channelParam = interaction.options.getChannel('канал')
+
+    if (channelParam) {
+      if (
+        channelParam.type !== 'GUILD_TEXT' &&
+        channelParam.type !== 'GUILD_VOICE' &&
+        channelParam.type !== 'GUILD_PUBLIC_THREAD' &&
+        channelParam.type !== 'GUILD_PRIVATE_THREAD'
+      ) {
+        await respond({
+          embeds: [
+            Utils.generateErrorMessage(
+              'Необходимо указать текстовый канал.',
+              ErrorMessageType.Error
+            )
+          ]
+        })
+        return
+      }
+
+      if (channelParam.isText()) {
+        if (channelParam.guild.id === guild.id) {
+          textChannel = channelParam
+        } else {
+          await respond({
+            embeds: [
+              Utils.generateErrorMessage(
+                'Необходимо указать текстовый канал.',
+                ErrorMessageType.Error
+              )
+            ]
+          })
+          return
+        }
+      } else {
+        await respond({
+          embeds: [
+            Utils.generateErrorMessage(
+              'Не удалось найти такой канал.',
+              ErrorMessageType.Error
+            )
+          ]
+        })
+        return
+      }
+    }
+
+    player.setTextChannel(textChannel.id)
+
+    await respond({
+      embeds: [
+        Utils.generateErrorMessage(
+          `Для уведомлений установлен текстовый канал <#${textChannel.id}>`,
+          ErrorMessageType.NoTitle
+        )
+      ]
+    })
+  }
+})
