3f246df009bba5a71fe90027f0a8e4d075631c3c
Nikita <n-bogun@mail.ru>
2022-12-07 16:48:10 +0300

    pretty print logs in dev env

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,11 +1,19 @@
 import pino from 'pino'
 
-const logger = pino(
-  pino.destination({
+let transport
+
+if (process.env.NODE_ENV === 'development') {
+  transport = pino.transport({
+    target: 'pino-pretty'
+  })
+} else {
+  transport = pino.destination({
     minLength: 256,
     sync: false
   })
-)
+}
+
+const logger = pino(transport)
 logger.info('Logger initialized.')
 
 export default logger
