a04d0feedfd3b0964ddb4030142ee32e8fded2db
nik9 <n-bogun@mail.ru>
2023-10-08 15:55:51 +0300

    update workaround

--- src/modules/botPlayer.ts
+++ src/modules/botPlayer.ts
@@ -21,7 +21,6 @@ export default class BotPlayer {
   public stopped: boolean
   public reconnecting: boolean
 
-  private latestStartEvent: Date | null = null
   private latestEndEvent: Date | null = null
 
   private fastEndEventsCount = 0
@@ -51,8 +50,6 @@ export default class BotPlayer {
     })
     this.player
       .on('start', async (data) => {
-        this.latestStartEvent = new Date()
-
         logger.info(
           {
             guild_id: data.guildId,
@@ -102,16 +99,15 @@ export default class BotPlayer {
 
         // temp workaround for end/start event spam
         // TODO: fix this shit
-        this.latestEndEvent = new Date()
+        const eventDate = new Date()
 
-        if (
-          this.latestStartEvent &&
-          this.latestEndEvent.getTime() - this.latestStartEvent.getTime() <= 200
-        ) {
+        if (this.latestEndEvent && eventDate.getTime() - this.latestEndEvent.getTime() <= 1000) {
           this.fastEndEventsCount++
         } else {
           this.fastEndEventsCount = 0
         }
+
+        this.latestEndEvent = eventDate
 
         if (this.fastEndEventsCount >= 5) {
           this.fastEndEventsCount = 0
@@ -125,6 +121,25 @@ export default class BotPlayer {
             },
             'Crazy player destroyed'
           )
+
+          const channel = this.client.channels.cache.get(this.textChannelId)
+          if (!channel?.isTextBased()) return
+
+          await Utils.sendMessageToChannel(
+            channel,
+            {
+              embeds: [
+                Utils.generateErrorMessage(
+                  'Бот столкнулся с проблемой и был вынужден выйти из канала. Пожалуйста, запустите бота заново. ' +
+                    'Разработчики уже знают об этой проблеме. ' +
+                    'Если бот все ещё не работает, напишите в [группу ВК](https://vk.com/vkmusicbotds) или на [сервер Discord](https://discord.com/invite/3ts2znePu7).',
+                  ErrorMessageType.Warning
+                )
+              ]
+            },
+            30_000
+          )
+
           return
         }
 
